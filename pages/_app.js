5b21c3fa3a07574ca05928caf61fcf73dcf4a992
Nander <nander@nander.dev>
2020-11-10 17:20:55 +0100

    🚑 fix theme

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import { ThemeProvider } from "next-themes";
 
 function Nander({ Component, pageProps }) {
   return (
-    <ThemeProvider>
+    <ThemeProvider defaultTheme="system">
       <Component {...pageProps} />
     </ThemeProvider>
   );
