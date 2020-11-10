820241138e967c13fcb470b6bf83250d1ee673f3
Nander <nander@nander.dev>
2020-11-10 17:09:46 +0100

    ⚡️ PERFECT LIGHTHOUSE SCOREEEEEEE

---
+++ components/Github/Profile.js
@@ -0,0 +1,15 @@
+import styles from "../../styles/Profile.module.scss";
+
+export default function Profile(props) {
+  const { data } = props;
+  console.log(data);
+
+  return (
+    <div className={styles.profile}>
+      <div className={styles.data}>
+        <p className={styles.text}>Followers: {data.followers}</p>
+        <p className={styles.text}>Repos: {data.public_repos}</p>
+      </div>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from "next/head";
+import Image from "next/image";
 import useSWR from "swr";
+import Profile from "../components/Github/Profile";
 import styles from "../styles/Home.module.scss";
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
@@ -12,7 +14,6 @@ export default function Home() {
         headers: { Accept: "application/vnd.github.v3+json" },
       })
   );
-
   return (
     <div className={styles.container}>
       <Head>
@@ -22,12 +23,31 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <div className={styles.left}>
-          <p>I'm Nander I like to code I like the color indigo</p>
+        <div className={styles.side}>
+          <div>
+            <Image
+              src="https://github.com/nanderLP.png"
+              alt="GitHub Profile Picture"
+              width="200px"
+              height="200px"
+            />
+          </div>
+          <div>
+            <p>I'm Nander</p>
+            <p>I like to code</p>
+            <p>
+              I like the color <span style={{ color: "var(--primary)" }}>indigo</span>
+            </p>
+          </div>
         </div>
-        <div className={styles.right}>
-          <p>{data ? data.followers : "Loading"}</p>
-        </div>
+        {data ? (
+          <div className={styles.side}>
+            <a href={data.html_url} target="_blank" rel="noreferrer">
+              <h2 className={styles.ghTitle}>My GitHub Profile</h2>
+            </a>
+            <Profile data={data} />
+          </div>
+        ) : null}
       </main>
     </div>
   );
