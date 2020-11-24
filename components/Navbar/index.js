1976f18af3d8fc5620e79a83402aa386420a0c06
Nander <nander@nander.dev>
2020-11-24 20:21:32 +0100

    🎨 add navbar and favicon

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -2,7 +2,7 @@ import styles from "./Navbar.module.scss";
 import { useTheme } from "next-themes";
 import Link from "next/link";
 
-import { Music, Moon, Sun, Info, Mail } from "react-feather";
+import { Music, Moon, Sun, Info, Mail, Home } from "react-feather";
 
 export default function Navbar() {
   const { theme, setTheme } = useTheme();
@@ -17,6 +17,13 @@ export default function Navbar() {
   return (
     <nav className={styles.nav}>
       <div className={styles.navItem}>
+        <Link href="/">
+          <a className={styles.a}>
+            <Home size="2rem" />
+          </a>
+        </Link>
+      </div>
+      <div className={styles.navItem}>
         <div
           onClick={toggleTheme}
           style={{ background: "none", border: "none", cursor: "pointer" }}
@@ -26,20 +33,20 @@ export default function Navbar() {
       </div>
       <div className={styles.navItem}>
         <Link href="/music">
-          <a>
+          <a className={styles.a}>
             <Music size="2rem" />
           </a>
         </Link>
       </div>
       <div className={styles.navItem}>
-        <a href="mailto:nander@nander.dev">
+        <a href="mailto:nander@nander.dev" className={styles.a}>
           <Mail size="2rem" />
         </a>
       </div>
       <div className={styles.navItem}>
-        <Link href="/music">
-          <a>
-            <Music size="2rem" />
+        <Link href="/credits">
+          <a className={styles.a}>
+            <Info size="2rem" />
           </a>
         </Link>
       </div>
--- pages/index.js
+++ pages/index.js
@@ -16,33 +16,26 @@ export default function Home() {
   );
   return (
     <div>
-      <Head>
-        <title>Nander</title>
-        <link rel="icon" href="/favicon.ico" />
-        <meta name="Description" content="homepage of nander."></meta>
-      </Head>
       <main className={styles.main}>
-        <div className={styles.center}>
-          <div className={styles.side}>
-            <Picture />
-            <div>
-              <p>I'm Nander</p>
-              <p>I like to code</p>
-              <p>
-                I like the color{" "}
-                <span style={{ color: "var(--primary)" }}>indigo</span>
-              </p>
-            </div>
+        <div className={styles.side}>
+          <Picture />
+          <div>
+            <p>I'm Nander</p>
+            <p>I like to code</p>
+            <p>
+              I like the color{" "}
+              <span style={{ color: "var(--primary)" }}>indigo</span>
+            </p>
           </div>
-          {data ? (
-            <div className={styles.side}>
-              <a href={data.html_url} target="_blank" rel="noreferrer">
-                <h2 className={styles.ghTitle}>My GitHub Profile</h2>
-              </a>
-              <Profile data={data} />
-            </div>
-          ) : null}
         </div>
+        {data ? (
+          <div className={styles.side}>
+            <a href={data.html_url} target="_blank" rel="noreferrer">
+              <h2 className={styles.ghTitle}>My GitHub Profile</h2>
+            </a>
+            <Profile data={data} />
+          </div>
+        ) : null}
       </main>
     </div>
   );
