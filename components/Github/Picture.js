f2d1e3c30efbf206f593f6af29e2e77402ab9597
Nander <nander@nander.dev>
2020-12-19 19:09:57 +0100

    :lipstick: make profile picture rounded and fix responsiveness

--- components/Github/Picture.js
+++ components/Github/Picture.js
@@ -1,5 +1,6 @@
 import Cat from "../Cat/Cat";
 import Image from "next/image";
+import styles from "./Picture.module.scss";
 
 export default function Picture() {
   return (
@@ -8,9 +9,10 @@ export default function Picture() {
       <Image
         src="https://github.com/nanderLP.png"
         alt="GitHub Profile Picture"
-        width="200px"
-        height="200px"
+        width={200}
+        height={200}
         priority="true"
+        className={styles.img}
       />
     </div>
   );
